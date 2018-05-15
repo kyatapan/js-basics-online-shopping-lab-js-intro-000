03baeb3d69a0e7273e1841c9ba20e94a2d30d361
kathrin v <go.kathrin@gmail.com>
2018-05-15 19:03:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,15 +10,34 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  let randomPrice = Math.floor(Math.random()*100);
+var newItem = {'itemName': item, 'itemPrice': randomPrice };
+cart.push(newItem);
+return `${newItem.itemName} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+  if (!getCart().length) {
+    return `Your shopping cart is empty.`;
+  }
+  else {
+    const itemsAndPrices = [];
+    const cartSize = getCart().length;
+     for (let i=0; i < cartSize-1; i++) {
+    itemsAndPrices.push(`${cart[i].itemName} at $${cart[i].itemPrice}`);
+  }
+
+  return `In your cart, you have ${itemsAndPrices.join(', ')}, and ${cart[cartSize-1].itemName} at $${cart[cartSize-1].itemPrice}.`;
+}
 }
 
 function total() {
-  // write your code here
+    let totalCost = 0;
+    for (let i=0; i < getCart().length; i++) {
+    totalCost += cart[i].itemPrice;
+  }
+
+  return totalCost;
 }
 
 function removeFromCart(item) {
